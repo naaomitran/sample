ad483353a96b0c8cf05b665f7bbd78e3a92b87d2
Naomi Tran <naaomitran@gmail.com>
2020-12-04 22:12:52 -0800

    Changed dir1

---
+++ dir1/app1.java
@@ -0,0 +1,15 @@
+package dir1;
+
+public class app1 {
+    private int num;
+
+    public app1() {
+        num = 3;
+    }
+
+    private int addition(int toThis) {
+        int ans = num + toThis;
+        return ans;
+    }
+    
+}
